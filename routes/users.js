6659242eeaf9eb470b5b2e38b2043048d4b82c6d
Andrew <acreyes311@yahoo.com>
2020-07-22 13:52:56 -0700

    Register/HashPW, JWT, Auth Route

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../models/User');
@@ -17,13 +20,55 @@ router.post(
       'Please enter a password with 6 or more characters'
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
+    const { name, email, password } = req.body;
 
-    res.send('passed');
+    try {
+      // Check if user email already exists
+      let user = await User.findOne({ email }); // mongoose method
+      if (user) {
+        return res.status(400).json({ msg: 'User already exists ' });
+      }
+      // If not store User model info in user
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      // bcrypt password
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      // save into DB
+      await user.save();
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (error) {
+      console.error(error);
+      res.satus(500).send('Server Error');
+    }
   }
 );
 
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const express = require('express'); // common js (not import)
+// CommonJS module system (not import)
+const express = require('express');
 const connectDB = require('./config/db');
 
 const app = express();
