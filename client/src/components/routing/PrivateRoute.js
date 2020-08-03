ba5197eccd2d5b68b7808772bbaf8f2dde8652a8
Andrew <acreyes311@yahoo.com>
2020-08-03 16:31:16 -0700

    PrivateRoute component to redirect to login

---
+++ client/src/components/routing/PrivateRoute.js
@@ -0,0 +1,26 @@
+import React, { useContext } from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import AuthContext from '../../context/auth/authContext';
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const authContext = useContext(AuthContext);
+  const { isAuthenticated, loading } = authContext;
+
+  return (
+    // if user is not authenticated and state is done loading
+    // then Redirect to login page
+    // else load component and extra props in component
+    <Route
+      {...rest}
+      render={(props) =>
+        !isAuthenticated && !loading ? (
+          <Redirect to='/login' />
+        ) : (
+          <Component {...props} />
+        )
+      }
+    />
+  );
+};
+
+export default PrivateRoute;
